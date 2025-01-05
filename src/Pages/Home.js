fc2982f84918c4dddd9b91998154c7edbe0b1565
ashutoshqsofte <ashutosh@qsofte.com>
2025-01-06 00:40:35 +0530

    2nd-Update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,29 @@
 import "../PagesCss/Home.css";
+import logo from "../Logo/logo.png";
 
 function Home() {
   return (
     <div className="main-home">
-      <div className="Fst-home"></div>
+      <div className="Fst-home">
+        <div className="logo-box">
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ width: "150px", height: "auto" }}
+          />
+          <div>
+            <h1 style={{ color: 'white', margin: '20px 0 10px', textAlign: 'center' }}>webService</h1>
+          </div>
+        </div>
+        <div className="Fsttxt-box">
+          <h1 className="Fst-hng">
+            Your Dream Website, Designed for Your Business
+          </h1>
+          <h3 className="Sec-hng">
+            Responsive, user-friendly, and designed to drive results.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
